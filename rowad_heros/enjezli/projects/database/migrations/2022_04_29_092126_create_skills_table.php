77b4c95ce51f2e6b3ae58a81a7eb512b52fd2735
Hertz-H <afaf14albakri@gmail.com>
2022-04-29 13:06:19 +0300

    skills updated

--- rowad_heros/enjezli/projects/database/migrations/2022_04_29_092126_create_skills_table.php
+++ rowad_heros/enjezli/projects/database/migrations/2022_04_29_092126_create_skills_table.php
@@ -16,6 +16,7 @@ class CreateSkillsTable extends Migration
         Schema::create('skills', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->integer('parent_id');
             $table->timestamps();
         });
     }
