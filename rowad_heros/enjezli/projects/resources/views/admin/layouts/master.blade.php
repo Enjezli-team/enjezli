cd2ccad609723f0372e91926ca0be086c22b78ec
shebaal <alshamery.sheba@gmail.com>
2022-04-30 23:22:10 +0300

    install bootstrap icon

--- rowad_heros/enjezli/projects/resources/views/admin/layouts/master.blade.php
+++ rowad_heros/enjezli/projects/resources/views/admin/layouts/master.blade.php
@@ -10,6 +10,7 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
   <!-- rtl css -->
   <link rel="stylesheet" href="admin/rtlCSS/custom.css">
   <!-- rtl style -->
@@ -222,13 +223,13 @@
           <li class="nav-item menu-open">
           
                 <a href="./index.html" class="nav-link active">
-                    <img src="admin/svg/home-outline.svg" />
+                    <i class="bi bi-house"></i>
                   <p>الرئيسية </p>
                 </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-                <img src="admin/svg/offer.svg" />
+                <i class="bi bi-wallet"></i>
               <p>
                العروض
 
@@ -238,8 +239,7 @@
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-          
-                <img src="admin/svg/projects.svg" />
+                <i class="bi bi-kanban"></i>
               <p>
                 المشاريع
                 <i class="fas fa-angle-left right"></i>
@@ -248,19 +248,18 @@
             <ul class="nav nav-treeview">
               <li class="nav-item">
                 <a href="pages/forms/general.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                    <i class="bi bi-folder-plus"></i>
                   <p>الجديده </p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="pages/forms/advanced.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                    <i class="bi bi-folder-symlink"></i>
                   <p>قيد التنفيذ </p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                <a href="" class="nav-link"><i class="bi bi-folder-x"></i>
                   <p>المحذوفة</p>
                 </a>
               </li>
@@ -269,15 +268,15 @@
         
               <li class="nav-item">
                 <a href="pages/tables/data.html" class="nav-link">
-               
-                  <i class="far fa-circle nav-icon"></i>
+                    <i class="bi bi-box-arrow-right"></i>
                   <p>المستخدمين</p>
                 </a>
               </li>
        
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-envelope"></i>
+                
+                <img src="admin/svg/report.svg" />
               <p>
                 التقارير
               </p>
@@ -287,7 +286,8 @@
           <li class="nav-item">
           
                 <a href="pages/examples/projects.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                 
+                <img src="admin/svg/chat.svg" />
                   <p>الدردشة</p>
                 </a>
           </li>
@@ -301,11 +301,7 @@
        
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M11.4927 2C13.9753 2 16 3.99 16 6.44V11.23H9.89535C9.45785 11.23 9.11192 11.57 9.11192 12C9.11192 12.42 9.45785 12.77 9.89535 12.77H16V17.55C16 20 13.9753 22 11.4724 22H6.51744C4.02471 22 2 20.01 2 17.56V6.45C2 3.99 4.03488 2 6.52762 2H11.4927ZM18.5402 8.5502C18.8402 8.2402 19.3302 8.2402 19.6302 8.5402L22.5502 11.4502C22.7002 11.6002 22.7802 11.7902 22.7802 12.0002C22.7802 12.2002 22.7002 12.4002 22.5502 12.5402L19.6302 15.4502C19.4802 15.6002 19.2802 15.6802 19.0902 15.6802C18.8902 15.6802 18.6902 15.6002 18.5402 15.4502C18.2402 15.1502 18.2402 14.6602 18.5402 14.3602L20.1402 12.7702H16.0002V11.2302H20.1402L18.5402 9.6402C18.2402 9.3402 18.2402 8.8502 18.5402 8.5502Z"
-                  fill="#8ecae6" />
-              </svg>
+                <i class="bi bi-activity"></i>
 
               <p class="text">تسجيل الخروج</p>
             </a>
