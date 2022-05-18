91843daac0f38648b9f2981a9ef5a5d2073102b0
Thoryaa <93941131+Thoryaa@users.noreply.github.com>
2022-05-18 23:08:04 +0300

    upload

--- resources/views/website/users/offers/index.blade.php
+++ resources/views/website/users/offers/index.blade.php
@@ -1,41 +1,234 @@
-@extends('website.users.user_dashboard.layout.master')
+<link rel="stylesheet" href="{{ asset('auth_assets/project_assests/css/project_card.css ') }}">
+<style>
+    /****search effect******/
+
+
+    /****search effect******/
+
+    .search {
+        cursor: pointer;
+        color: #186d80;
+        font-size: 18px;
+    }
+
+    .search-box {
+        width: fit-content;
+        height: fit-content;
+        position: relative;
+    }
+
+    .input-search {
+        height: 50px;
+        width: 50px;
+        border-style: none;
+        padding: 10px;
+        letter-spacing: 2px;
+        outline: none;
+        border-radius: 25px;
+        transition: all .5s ease-in-out;
+        background-color: transparent;
+        padding-right: 40px;
+        color: #257587;
+    }
+
+    .input-search::placeholder {
+        color: gray;
+        letter-spacing: 2px;
+    }
+
+    .btn-search {
+        width: 50px;
+        height: 50px;
+        border-style: none;
+        font-size: 16px;
+        font-weight: bold;
+        outline: none;
+        cursor: pointer;
+        border-radius: 50%;
+        position: absolute;
+        right: 0px;
+        color: black;
+        background-color: transparent;
+        pointer-events: painted;
+    }
+
+    .btn-search:focus~.input-search {
+        width: 177px;
+        border-radius: 0px;
+        background-color: transparent;
+        border-bottom: 1px solid gray;
+        transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+    }
+
+    .input-search:focus {
+        width: 177px;
+        border-radius: 0px;
+        background-color: transparent;
+        border-bottom: 1px solid gray;
+        transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+    }
+
+    @media (min-width:768px) {
+        .input-search:focus {
+            width: 170px;
+        }
+    }
+
+    .show_more {
+        background-color: #186d80;
+        color: white;
+        border-radius: 0.25rem;
+    }
+
+    .show_more:hover {
+        background-color: white;
+        color: #186d80;
+    }
+
+    .price {
+        font-size: 33px;
+        color: #186d80;
+    }
+
+</style>
+@extends('website.layouts.master_dash')
 @section('content')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="{{ asset('css/user_dashboard.css') }}">
-
-    <link rel="stylesheet" href="{{ asset('css/user_offer.css') }}">
-
-    <section class="home">
-        <div class="page_title">
-            <div class='title shadow'>
-
-                <span class='p_relative'>
-
-                    العروض
-                    <span class='myproject_count'>
-                        {{ $offers->count() }}
-
-                    </span>
-
-                </span>
-
-
+    <!-- End Navbar -->
+    <div class="container-fluid mt-5 ">
+        <div class="page-header min-height-300 border-radius-xl mt-4 text-center text-white d-flex justify-content-center"
+            style="">
+            <span class="mask bg-gradient-dark"></span>
+            <div class='text-center' style='z-index:12'>
+                <h3 class='text-white'>
+                 عرض العروض
+                 [                        {{ $offers->count() }}
+]
+                 </h3>
+                <p>
+تعرض هذه الصفحة العروض التي قدمت اليها
+                </p>
             </div>
-
         </div>
-        @if (!$offers->isEmpty())
+
+    </div>
+    <div class="container-fluid pb-4">
+        <div class="row">
+  @if (!$offers->isEmpty())
             @foreach ($offers as $offer)
-                <div class='porto_container'>
+            <div class="col-12 col-xl-4 mt-3 ">
+                <div class="card h-100 ">
+                      <div class="">
+                        <a href="projects/{{ $offer->sal_project_id->id }}#offer{{ $offer->id }}">
+                            <div class="personal_info_container myworks" style="width: auto;height:415px">
+                                <div class="container_card">
+                                    <div class="">
+                                        <h2 class="h4">{{ $offer->sal_project_id->title }}</h2>
+                                        <div class='mt-3 mb-3'>
+                                            <div class="flex  align-items-baseline gap-2">
+                                                <span>
+                                                    <ion-icon name="person-outline"></ion-icon>
+                                                </span>
+                                                <h5></h5>
+                                            </div>
+                                            <div class="flex gap-2">
+                                                <span>
+                                                    <ion-icon name="time-outline"></ion-icon>
+                                                </span>
+                                                <span class="aut_pub"></span>
+
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                          {{ Str::substr($offer->description, 0, 50) }}
+                                         ...معرفة المزيد 
 
 
+                                        </div>
+                                        <div class="liks_shows">
+                                            <ul class="d-grid w-100 gap-1 pe-0" >
+                                        <div class='d-flex w-100 justify-content-between align-items-center'>
+                                            <div>
+                                               <li>
+                                                    <a href="">
+                                                        <span  class="price">    {{ $offer->price }}$</span>
+                                                    </a>
+                                                </li>
+                                            </div>
+                                              
+                                              <div>
+                                                <li>
+                                                    <a href="" class="w-50">
+                                                        <span> الفترة</span>
+                                                        :
+                                                        <span>   {{ $offer->duration }}يوم</span>
+                                                    </a>
+                                                </li>
+                                             
 
-                    <div class=' porto shadow'>
+                                           
+                                              </div>
+                                        </div>
+                                                {{-- {{ $item->sal_skill}}
+                                <div class='skills ' style=''>
+                                    @foreach ($item->sal_skills_by as $skill)
+                                    {{ $skill->sal_skill->title }}<br>
+                                    @endforeach
+                                </div> --}}
+                                                <li class='d-flex gap-2'>
+                                                    <a href="" class="status">
+                                                    الحالة:
+                                                    
+                                              @if ($offer->sal_project_id->status == 1 && $offer->status == 0)
+                                            <a style="    color: #3a416f;
+    background: #fce4ec;
+    padding: 1px 12px;" class="status">ملغي</a>
+                                        @elseif($offer->sal_project_id->status == 1 && $offer->status == 1)
+                                            <a style="    color: #3a416f;
+    background: #c9e1e9;
+    padding: 1px 12px;" class="status">بانتظار الموافقة </a>
+                                        @elseif($offer->sal_project_id->status == 4 && $offer->status == 1)
+                                            <a style="    color: #3a416f;
+    background: #a5d6a7;
+    padding: 1px 12px;" class="status"> مقبول</a>
+                                        @elseif($offer->sal_project_id->status == 4 && $offer->status == 2)
+                                            <a style="    color: #3a416f;
+    background: #a5d6a7;
+    padding: 1px 12px;" class="status">تمت موافقتك </a>
+                                        @elseif($offer->sal_project_id->status == 2 && $offer->status == 3)
+                                            <a style="    color: #3a416f;
+    background: #4fc3f796;
+    padding: 1px 12px;" class="status">قيد التنفيذ </a>
+                                        @elseif($offer->sal_project_id->status == 3 && $offer->status == 3)
+                                            {{-- <a style="color:black" class="status">تم التسليم  </a> --}}
+                                            <a style="    color: #3a416f;
+    background: #4fc3f796;
+    padding: 1px 12px;" class="status"> قيد التسليم </a>
+                                        @elseif($offer->sal_project_id->status == 5 && $offer->status == 3)
+                                            <a style="    color: #3a416f;
+    background: #a5d6a7;
+    padding: 1px 12px;" class="status"> تم الاستلام </a>
+                                        @elseif($offer->sal_project_id->status == 1 && $offer->status == 4)
+                                            <a style="    color: #3a416f;
+    background: #fce4ec;
+    padding: 1px 12px;" class="status">مرفوض</a>
+                                        @endif
 
-                        <div class='edit'>
 
-                            @if ($offer->sal_project_id->status == 1 && $offer->status == 0)
+                                                    </a>
+                                                </li>
+                                           
+                                           
+                                            </ul>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="hr">
+                                    </div>
+                                    <div class="liks_shows">
+
+                                            @if ($offer->sal_project_id->status == 1 && $offer->status == 0)
                             @elseif($offer->sal_project_id->status == 1 && $offer->status == 1)
                                 {{-- <form action="{{ route('cancelOffer', $offer->id) }}" method="post">
                                     @csrf
@@ -236,91 +429,24 @@
                                 </div>
                             @endif
 
-                        </div>
-                        <a href="projects/{{ $offer->sal_project_id->id }}#offer{{ $offer->id }}">
+  
 
-                            <div class='img_container'>
-                                <div class='img_project'>
-                                </div>
-                                <div class='personal_info_first'>
-                                    <div class='phone'>
-                                        <i class='bx bx-time'></i> <span class='time'> 22/2/2 22:22pm
-                                        </span>
+  
+                                        
                                     </div>
-
                                 </div>
                             </div>
-
-
-
-
-                            <div class='personal_desc'>
-
-                                <div class='title_desc'>
-                                    <h5><i class='bx bxs-quote-right'></i></h5>
-
-                                    <span> {{ $offer->sal_project_id->title }}</span>
-                                    <h5><i class='bx bxs-quote-left'></i></h5>
-
-                                </div>
-                                <div class='desc'>
-                                    {{ Str::substr($offer->description, 0, 80) }}...
-                                </div>
-                                <div class='details'>
-                                    <div>
-                                        السعر
-                                        <span>
-                                            {{ $offer->price }}
-                                        </span>
-                                        $
-
-                                    </div>
-
-                                    <div>
-                                        الحالة
-                                        @if ($offer->sal_project_id->status == 1 && $offer->status == 0)
-                                            <a style="color:black" class="status">ملغي</a>
-                                        @elseif($offer->sal_project_id->status == 1 && $offer->status == 1)
-                                            <a style="color:black" class="status">بانتظار الموافقة </a>
-                                        @elseif($offer->sal_project_id->status == 4 && $offer->status == 1)
-                                            <a style="color:black" class="status"> مقبول</a>
-                                        @elseif($offer->sal_project_id->status == 4 && $offer->status == 2)
-                                            <a style="color:black" class="status">تمت موافقتك </a>
-                                        @elseif($offer->sal_project_id->status == 2 && $offer->status == 3)
-                                            <a style="color:black" class="status">قيد التنفيذ </a>
-                                        @elseif($offer->sal_project_id->status == 3 && $offer->status == 3)
-                                            {{-- <a style="color:black" class="status">تم التسليم  </a> --}}
-                                            <a style="color:black" class="status"> قيد التسليم </a>
-                                        @elseif($offer->sal_project_id->status == 5 && $offer->status == 3)
-                                            <a style="color:black" class="status"> تم الاستلام </a>
-                                        @elseif($offer->sal_project_id->status == 1 && $offer->status == 4)
-                                            <a style="color:black" class="status">مرفوض</a>
-                                        @endif
-
-
-
-                                    </div>
-                                    <div>
-                                        المدة
-                                        <span>
-                                            {{ $offer->duration }}
-                                        </span>
-                                        ايام
-                                    </div>
-
-                                </div>
-
-                            </div>
-
+                        </a>
                     </div>
-
-                    </a>
-
-                    <div>
-            @endforeach
+                </div>
+            </div>
+      @endforeach
         @endif
 
         </div>
 
 
-    </section>
+
+    </div>
+    </div>
+@endsection
